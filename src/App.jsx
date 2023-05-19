059bfadc3170157e6581c51ccc984962f3486ae0
ofwebdev <nkomorfaruk@gmail.com>
2023-05-19 16:06:39 +0600

    Tailwind and daisyui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1>Assignment 11</h1>
+      <Header />
     </>
   );
 }
